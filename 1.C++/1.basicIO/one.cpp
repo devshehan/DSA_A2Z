941798d5911225167568bfa0bfe3f3c0400cbb4d
devshehan <shehaan.avishka00@gmail.com>
2023-05-23 11:49:27 +0530

    basicIO datatypes ifelse

---
+++ 1.C++/1.basicIO/one.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World" << endl; 
+
+    int x, y; 
+    cin >> x >> y;
+
+    cout << "Value of x: " << x << " and y: " << y << endl;;
+}
