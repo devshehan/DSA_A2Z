a9df15f7faccb5f691811b3803c873bc5d8c93c9
devshehan <shehaan.avishka00@gmail.com>
2023-05-24 12:03:46 +0530

    list

---
+++ 1.2.STL/list.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    //list is same as the vector
+    // but it also has front end operations
+    // list -> doubly linked list
+    // vector -> singly linked list
+
+
+    list<int> ls;
+
+    ls.push_back(5);
+    ls.push_front(6);
+    ls.push_back(34);
+    ls.push_back(30);
+    ls.pop_back();
+    ls.pop_front();
+
+    for(list<int>::iterator k = ls.begin(); k != ls.end(); k++){
+        cout << *k << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
