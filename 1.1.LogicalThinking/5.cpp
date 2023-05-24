b8249a7984095a0a1bf01b239730bc411790350e
devshehan <shehaan.avishka00@gmail.com>
2023-05-24 10:00:49 +0530

    patterns logical thinking

--- 1.1.LogicalThinking/5.cpp
+++ 1.1.LogicalThinking/5.cpp
@@ -6,12 +6,7 @@ int main(){
 
     char x = '*';
 
-    for(int i=5; i>0; --i){
-        for(int j=0; j<i; ++j){
-            cout << x;
-        }
-        cout << endl;
-    }
+
 
     return 0;
 }
